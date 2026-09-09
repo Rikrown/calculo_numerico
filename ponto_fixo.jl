7460b80b54547e5dd9996813705be20da868e3bf
gabriel <258523334+gabrielpgm2005@users.noreply.github.com>
2026-09-09 15:45:36 -0300

    Alteração no método de ponto fixo Mudei o método de ponto fixo para fazer a contagem de iterações com o exercicio que a professora passou em aula Escolhi esse q(x) porque convergia (dá pra provar com aquele teorema)

--- ponto_fixo.jl
+++ ponto_fixo.jl
@@ -1,12 +1,15 @@
-f(xnovo) = sqrt(6 - xnovo)
+q(x) = -sqrt(10 - exp(-0.1 * x))
 
-xnovo = 1.5
-xvelho = 0
+xnovo = -3.0
+xvelho = 0.0
+k = 0
 
-while abs(xnovo - xvelho) > 0.01
-    f(xnovo)
+while abs(xnovo - xvelho) > 10 ^ -5
+    global k += 1
     global xvelho = xnovo
-    global xnovo = f(xnovo)
+    global xnovo = q(xnovo)
+
 end
 
-println(xnovo)
+println("raiz: ",xnovo)
+println("iterações: ",k)
