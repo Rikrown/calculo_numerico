730bb4513a69ddf58363cb3d776822924451b14c
Rikrown <20201100045@pq.uenf.br>
2026-09-12 18:39:08 -0300

    automatizando o gj

--- gaussjordan.jl
+++ gaussjordan.jl
@@ -1,82 +1,27 @@
 mat = Float64.([3 2 4 1; 1 1 2 2; 4 3 -2 3])
 
-fator = mat[2,1] / mat[1,1]
-for i in 1:4
-   mat[2,i] = mat[2,i] - mat[1,i]*fator
-   if abs(mat[2,i]) < 10 ^ -5
-    mat[2,i] = 0
-   end
+for y in 1:3 
+    for x in 1:3
+        if x != y
+        fator = mat[x,y] / mat[y,y]
+        for i in 1:4
+            mat[x,i] = mat[x,i] - mat[y,i]*fator
+        if abs(mat[x,i]) < 10 ^ -5
+            mat[x,i] = 0
+        end
+        end 
+        end
+    end
 end
-println(mat[2,1], " ", mat[2,2], " ", mat[2,3], " ", mat[2,4])
 
-fator = mat[3,1] / mat[1,1]
-for i in 1:4
-   mat[3,i] = mat[3,i] - mat[1,i]*fator
-   if abs(mat[3,i]) < 10 ^ -5
-    mat[3,i] = 0
-   end
+for x in 1:3
+    fator = mat[x,x]
+    for i in 1:4
+        mat[x,i] = mat[x,i] / fator
+        if abs(mat[x,i]) < 10^-5
+            mat[x,i] = 0
+        end
+    end
 end
-println(mat[3,1], " ", mat[3,2], " ", mat[3,3], " ", mat[3,4])
 
-fator = mat[1,2] / mat[2,2]
-for i in 1:4
-   mat[1,i] = mat[1,i] - mat[2,i]*fator
-   if abs(mat[1,i]) < 10 ^ -5
-    mat[1,i] = 0
-   end
-end
-println(mat[1,1], " ", mat[1,2], " ", mat[1,3], " ", mat[1,4])
-
-fator = mat[3,2] / mat[2,2]
-for i in 1:4
-   mat[3,i] = mat[3,i] - mat[2,i]*fator
-   if abs(mat[3,i]) < 10 ^ -5
-    mat[3,i] = 0
-   end
-end
-println(mat[3,1], " ", mat[3,2], " ", mat[3,3], " ", mat[3,4])
-
-fator = mat[1,3] / mat[3,3]
-for i in 1:4
-   mat[1,i] = mat[1,i] - mat[3,i]*fator
-   if abs(mat[1,i]) < 10 ^ -5
-    mat[1,i] = 0
-   end
-end
-println(mat[1,1], " ", mat[1,2], " ", mat[1,3], " ", mat[1,4])
-
-fator = mat[2,3] / mat[3,3]
-for i in 1:4
-   mat[2,i] = mat[2,i] - mat[3,i]*fator
-   if abs(mat[2,i]) < 10 ^ -5
-    mat[2,i] = 0
-   end
-end
-println(mat[2,1], " ", mat[2,2], " ", mat[2,3], " ", mat[2,4])
-
-fator = mat[1,1]
-for i in 1:4
-   mat[1,i] = mat[1,i] / fator
-   if abs(mat[1,i]) < 10 ^ -5
-    mat[1,i] = 0
-   end
-end
-println(mat[1,1], " ", mat[1,2], " ", mat[1,3], " ", mat[1,4])
-
-fator = mat[2,2]
-for i in 1:4
-   mat[2,i] = mat[2,i] / fator
-   if abs(mat[2,i]) < 10 ^ -5
-    mat[2,i] = 0
-   end
-end
-println(mat[2,1], " ", mat[2,2], " ", mat[2,3], " ", mat[2,4])
-
-fator = mat[3,3]
-for i in 1:4
-   mat[3,i] = mat[3,i] / fator
-   if abs(mat[3,i]) < 10 ^ -5
-    mat[3,i] = 0
-   end
-end
-println(mat[3,1], " ", mat[3,2], " ", mat[3,3], " ", mat[3,4])
+println(mat)
